f01613afda6a72de01719d9cacc3560e542109ee
Old <225056366+roy-henderson@users.noreply.github.com>
2025-11-23 16:15:36 -0500

    Add mobile-optimized component: index.ts

---
+++ components/mobile/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Mobile Components Library
+ * CR AudioViz AI
+ * 
+ * Import components from here:
+ * import { MobileNav, MobileButton, MobileInput } from '@/components/mobile';
+ */
+
+export { MobileNav } from './MobileNav';
+export type { NavLink } from './MobileNav';
+
+export { MobileButton, MobileButtonGroup } from './MobileButton';
+export type { MobileButtonProps } from './MobileButton';
+
+export { MobileInput, MobileTextarea, MobileSelect } from './MobileInput';
+export type { MobileInputProps, MobileTextareaProps, MobileSelectProps } from './MobileInput';
+
+export { MobileCard, MobileCardHeader, MobileCardFooter, MobileModal } from './MobileCard';
+export type { MobileCardProps, MobileCardHeaderProps, MobileCardFooterProps, MobileModalProps } from './MobileCard';
+
+export { MobileTable, StatusBadge } from './MobileTable';
+export type { MobileTableProps, MobileTableColumn, StatusBadgeProps } from './MobileTable';
